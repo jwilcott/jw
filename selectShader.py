2f1feb3f21778632cee94bbf2d33bbae6f0a9194
Justin <jwilcott@gmail.com>
2025-04-24 16:59:30 -0700

    fixed error if two objects have the same name in the scene

--- selectShader.py
+++ selectShader.py
@@ -4,7 +4,8 @@ import maya.cmds as cmds
 
 def select_shader_from_selection():
     """Selects the shader(s) attached to the currently selected object(s)."""
-    selected_objects = cmds.ls(selection=True, type='transform')
+    # Use long=True to get unique path names
+    selected_objects = cmds.ls(selection=True, long=True, type='transform')
 
     if not selected_objects:
         cmds.warning("Please select an object.")
@@ -12,18 +13,24 @@ def select_shader_from_selection():
 
     shaders_to_select = []
     for obj in selected_objects:
-        shapes = cmds.listRelatives(obj, shapes=True, type='shape')
+        # Get full paths for shapes to ensure uniqueness
+        shapes = cmds.listRelatives(obj, shapes=True, type='shape', fullPath=True)
         if shapes:
-            for shape in shapes:
+            for shape in shapes: # 'shape' now holds the full path
+                # Use the full path of the shape for connection queries
                 shading_groups = cmds.listConnections(shape, type='shadingEngine')
                 if shading_groups:
+                    # Shading group names are usually unique, but check connections from them
                     for sg in shading_groups:
-                        shaders = cmds.listConnections(sg + ".surfaceShader")
+                        # Ensure we query the specific surfaceShader attribute
+                        shaders = cmds.listConnections(sg + ".surfaceShader", source=True, destination=False)
                         if shaders:
-                            shaders_to_select.extend(shaders)
+                            # Use list() to ensure we get unique shader names, though listConnections usually handles this
+                            shaders_to_select.extend(list(set(shaders))) # Use set to ensure uniqueness
 
     if shaders_to_select:
-        cmds.select(shaders_to_select, replace=True)
+        # Ensure the final list is unique before selecting
+        cmds.select(list(set(shaders_to_select)), replace=True)
     else:
         cmds.warning("No shaders found attached to the selected object(s).")
 
