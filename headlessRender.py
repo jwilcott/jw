22ad9733d03b2be2c6c61e48ef6a9e28019b5924
Justin <jwilcott@gmail.com>
2026-01-15 16:08:37 -0800

    remove unused GPU ids and print statements

--- headlessRender.py
+++ headlessRender.py
@@ -1,9 +1,9 @@
 import maya.cmds as cmds
 import subprocess
 
-# Specify GPUs to use (edit as needed, e.g., "0,1")
-gpu_ids = "0"
+# Specify by frame
 by_frame = "1"
+
 # Grab the current open Maya scene filepath
 current_file = cmds.file(q=True, sceneName=True)
 
@@ -12,13 +12,9 @@ if current_file:
     start_frame = int(cmds.getAttr("defaultRenderGlobals.startFrame"))
     end_frame = int(cmds.getAttr("defaultRenderGlobals.endFrame"))
     
-    # Command to render with selected GPU(s) and every 30th frame
+    # Command to render and every nth frame
     render_cmd = f'start cmd /k Render -s {start_frame} -e {end_frame} -b {by_frame} "{current_file}"'
 
     # Run the subprocess
     subprocess.Popen(render_cmd, shell=True)
 
-    print(f'Started headless Redshift render on GPU(s) {gpu_ids} for file:\n{current_file}')
-    print(f'Rendering frames {start_frame} to {end_frame} by {by_frame}')
-else:
-    print("No Maya scene file is open! Please save your file first.")
