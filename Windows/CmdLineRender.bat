264a20557535794a21b27f15dfa22167d17c9393
Justin <jwilcott@gmail.com>
2026-03-20 10:02:58 -0700

    Add CmdLineRender.bat script for batch rendering of Maya projects

---
+++ Windows/CmdLineRender.bat
@@ -0,0 +1,7 @@
+REM Render = C:\Program Files\Autodesk\Maya2025\bin
+
+Render "H:/Shared drives/Roblox Creative Studio/Projects/Social3D/makeup/030_Bacon_v04.mb"
+Render "H:/Shared drives/Roblox Creative Studio/Projects/Social3D/makeup/050_Pizza_v05.mb"
+Render "H:/Shared drives/Roblox Creative Studio/Projects/Social3D/makeup/040_Disco_v05.mb"
+Render "H:/Shared drives/Roblox Creative Studio/Projects/Social3D/makeup/040_Disco_Element_v02.mb"
+Render "H:/Shared drives/Roblox Creative Studio/Projects/Social3D/makeup/060_Cool_v06.mb"
