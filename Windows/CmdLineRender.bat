6fc470f9340204ad000f54e72889b5f9b95946f3
Justin <jwilcott@gmail.com>
2026-03-20 19:25:06 -0700

    select all keyframes

--- Windows/CmdLineRender.bat
+++ Windows/CmdLineRender.bat
@@ -1,7 +1,3 @@
 REM Render = C:\Program Files\Autodesk\Maya2025\bin
 
-Render "H:/Shared drives/Roblox Creative Studio/Projects/Social3D/makeup/030_Bacon_v04.mb"
-Render "H:/Shared drives/Roblox Creative Studio/Projects/Social3D/makeup/050_Pizza_v05.mb"
-Render "H:/Shared drives/Roblox Creative Studio/Projects/Social3D/makeup/040_Disco_v05.mb"
-Render "H:/Shared drives/Roblox Creative Studio/Projects/Social3D/makeup/040_Disco_Element_v02.mb"
-Render "H:/Shared drives/Roblox Creative Studio/Projects/Social3D/makeup/060_Cool_v06.mb"
+Render "H:/Shared drives/Roblox Creative Studio/Projects/Social3D/makeup/130_CloseUps_v03.mb"
